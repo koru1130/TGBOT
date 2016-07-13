66559fc8a2a07adcc52fc7ce937ea75d51974061
koru1130 <koru1130@gmail.com>
2016-07-13 10:09:22 +0000

    /out -> /

--- example.js
+++
@@ -1,8 +0,0 @@
-var TGBOT = require("./tg");
-var bot = new TGBOT({token:''});
-
-bot.addCmd('reply',function(toolBox,args){
-    var text = args[0] || "Hello";
-    toolBox.replyMsg(text);
-},"reply a message","/reply Msg");
-bot.start();

85f045c19b8caa57eca6fd82b0a2e37e54cd6fc4
koru1130 <koru1130@gmail.com>
2016-07-13 08:16:18 +0000

    update example.js

--- example.js
+++ example.js
@@ -1,8 +1,8 @@
 var TGBOT = require("./tg");
 var bot = new TGBOT({token:''});
 
-bot.addCmd('reply',function(args,toolBox){
+bot.addCmd('reply',function(toolBox,args){
     var text = args[0] || "Hello";
     toolBox.replyMsg(text);
-});
+},"reply a message","test");
 bot.start();

f6dbeb8d73d882ce8021e8ffece5429f4b3f5f13
koru1130 <koru1130@gmail.com>
2016-07-24 02:40:35 +0000

    args變成從1開始

--- example.js
+++ example.js
@@ -2,7 +2,7 @@ var TGBOT = require("./tg");
 var bot = new TGBOT({token:''});
 
 bot.addCmd('reply',function(toolBox,args){
-    var text = args[0] || "Hello";
+    var text = args[1] || "Hello";
     toolBox.replyMsg(text);
 },"reply a message","/reply Msg");
 bot.start();
